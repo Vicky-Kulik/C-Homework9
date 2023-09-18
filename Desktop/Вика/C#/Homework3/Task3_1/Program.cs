895c192a1523d23d7ec52706e09eafde9b10576f
Vika Kulik <viktoriya.onegina@gmail.com>
2023-09-18 23:36:24 +0300

    Homework3_1

---
+++ Desktop/Вика/C#/Homework3/Task3_1/Program.cs
@@ -0,0 +1,24 @@
+﻿//Напишите программу, которая принимает на вход
+//пятизначное число и проверяет, является ли оно палиндромом.
+
+ void Palindrom ()
+{
+    Console.Write("Введите пятизначное число: ") ;
+    string str = (Console.ReadLine ()) ;
+    
+    if (str[0] == str [4] && str[1] == str [3] && str.Length == 5 )
+            {
+            Console.WriteLine($"Число {str} палиндром");
+            }
+
+    if (str[0] != str [4] && str[1] != str [3] && str.Length == 5 )
+            {
+            Console.WriteLine($"Число {str} не является палиндром");
+            }
+    if (str.Length != 5 )
+             {
+                Console.WriteLine("Вы ввели не пятизначное число");
+             }
+}
+
+Palindrom ();
